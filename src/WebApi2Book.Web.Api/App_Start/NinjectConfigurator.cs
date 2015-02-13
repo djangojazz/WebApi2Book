036a7027edc7746fe29b6c392cfa32cba8031fc5
djangojazz <brettmorin2@gmail.com>
2015-02-13 11:58:13 -0800

    Change in configurator

--- src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -4,12 +4,14 @@ using NHibernate;
 using NHibernate.Context;
 using Ninject.Activation;
 using Ninject.Web.Common;
+using WebApi2Book.Data.QueryProcessors;
 using WebApi2Book.Data.SqlServer.Mapping;
 using log4net.Config;
 using Ninject;
 using WebApi2Book.Common;
 using WebApi2Book.Common.Security;
 using WebApi2Book.Common.Logging;
+using WebApi2Book.Data.SqlServer.QueryProcessors;
 using WebApi2Book.Web.Common;
 using WebApi2Book.Web.Common.Security;
 
@@ -29,6 +31,7 @@ namespace WebApi2Book.Web.Api
             ConfigureNHibernate(container);
 
             container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
+            container.Bind<IAddTaskQueryProcessor>().To<AddTaskQueryProcessor>().InRequestScope();
         }
 
         private void ConfigureLog4net(IKernel container)
