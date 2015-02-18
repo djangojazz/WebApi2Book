adad75b5a58162ef0d5d214a849e1b2b9d5f7189
djangojazz <brettmorin2@gmail.com>
2015-02-18 13:16:25 -0800

    Changes to TypeMapping Configuration

--- src/WebApi2Book.Web.Api/AutoMappingConfiguration/NewTaskToTaskEntityAutoMapperTypeConfigurator.cs
+++ src/WebApi2Book.Web.Api/AutoMappingConfiguration/NewTaskToTaskEntityAutoMapperTypeConfigurator.cs
@@ -4,19 +4,18 @@ using WebApi2Book.Web.Api.Models;
 
 namespace WebApi2Book.Web.Api.AutoMappingConfiguration
 {
-    public class NewTaskV2ToTaskEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    public class NewTaskVToTaskEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
     {
         public void Configure()
         {
-            Mapper.CreateMap<NewTaskV2, Data.Entities.Task>()
+            Mapper.CreateMap<NewTask, Data.Entities.Task>()
                 .ForMember(opt => opt.Version, x => x.Ignore())
                 .ForMember(opt => opt.CreatedBy, x => x.Ignore())
                 .ForMember(opt => opt.TaskId, x => x.Ignore())
                 .ForMember(opt => opt.CreatedDate, x => x.Ignore())
                 .ForMember(opt => opt.CompletedDate, x => x.Ignore())
                 .ForMember(opt => opt.Status, x => x.Ignore())
-                .ForMember(opt => opt.Users,
-                    x => x.ResolveUsing(newTask => newTask.Assignee == null ? null : new[] { newTask.Assignee }));
+                .ForMember(opt => opt.Users, x => x.Ignore());
         }
     }
 }
