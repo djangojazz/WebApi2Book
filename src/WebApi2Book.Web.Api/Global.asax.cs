7e4ec1fbcd6b03106b70a470f2825956925c23b3
djangojazz <brettmorin2@gmail.com>
2015-02-25 14:53:21 -0800

    More security

--- src/WebApi2Book.Web.Api/Global.asax.cs
+++ src/WebApi2Book.Web.Api/Global.asax.cs
@@ -3,17 +3,30 @@ using System.Web.Http;
 using WebApi2Book.Common.Logging;
 using WebApi2Book.Common.TypeMapping;
 using WebApi2Book.Web.Api.App_Start;
+using WebApi2Book.Web.Api.Security;
 using WebApi2Book.Web.Common;
 
 namespace WebApi2Book.Web.Api
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            new AutoMapperConfigurator().Configure(WebContainerManager.GetAll<IAutoMapperTypeConfigurator>());
+            RegisterHandlers();
+
+            new AutoMapperConfigurator().Configure(
+                WebContainerManager.GetAll<IAutoMapperTypeConfigurator>());
+        }
+
+        private void RegisterHandlers()
+        {
+            var logManager = WebContainerManager.Get<ILogManager>();
+
+            GlobalConfiguration.Configuration.MessageHandlers.Add(
+                new BasicAuthenticationMessageHandler(logManager,
+                    WebContainerManager.Get<IBasicSecurityService>()));
         }
 
         protected void Application_Error()
