6890574d32632943c8bdbb5a585d674e476f2b55
djangojazz <brettmorin2@gmail.com>
2015-03-24 12:53:48 -0700

    Added link logic

--- src/WebApi2Book.Web.Api/InquiryProcessing/AllTasksInquiryProcessor.cs
+++ src/WebApi2Book.Web.Api/InquiryProcessing/AllTasksInquiryProcessor.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using FluentNHibernate.Utils;
-using Ninject.Activation;
 using WebApi2Book.Common.TypeMapping;
 using WebApi2Book.Data;
 using WebApi2Book.Data.QueryProcessors;
+using WebApi2Book.Web.Api.LinkServices;
 using WebApi2Book.Web.Api.Models;
 using PagedTaskDataInquiryResponse = WebApi2Book.Web.Api.Models.PagedDataInquiryResponse<WebApi2Book.Web.Api.Models.Task>;
 
@@ -12,13 +11,20 @@ namespace WebApi2Book.Web.Api.InquiryProcessing
 {
     public class AllTasksInquiryProcessor : IAllTasksInquiryProcessor
     {
-        private readonly IAutoMapper _autoMapper;
-        private readonly IAllTasksQueryProcessor _queryProcessor;
+        public const string QueryStringFormat = "pagenumber={0}&pagesize={1}";
 
-        public AllTasksInquiryProcessor(IAllTasksQueryProcessor queryProcessor, IAutoMapper autoMapper)
+        private readonly IAutoMapper _autoMapper;
+        private readonly ICommonLinkService _commonLinkService;
+        private readonly IAllTasksQueryProcessor _queryProcessor;
+        private readonly ITaskLinkService _taskLinkService;
+
+        public AllTasksInquiryProcessor(IAllTasksQueryProcessor queryProcessor, IAutoMapper autoMapper,
+            ITaskLinkService taskLinkService, ICommonLinkService commonLinkService)
         {
             _queryProcessor = queryProcessor;
             _autoMapper = autoMapper;
+            _taskLinkService = taskLinkService;
+            _commonLinkService = commonLinkService;
         }
 
         public PagedTaskDataInquiryResponse GetTasks(PagedDataRequest requestInfo)
@@ -35,14 +41,57 @@ namespace WebApi2Book.Web.Api.InquiryProcessing
                 PageSize = requestInfo.PageSize
             };
 
+            if (!requestInfo.ExcludeLinks)
+            {
+                AddLinksToInquiryResponse(inquiryResponse);
+            }
+
             return inquiryResponse;
+        }
+
+        public virtual void AddLinksToInquiryResponse(PagedTaskDataInquiryResponse inquiryResponse)
+        {
+            inquiryResponse.AddLink(_taskLinkService.GetAllTasksLink());
+
+            _commonLinkService.AddPageLinks(inquiryResponse, GetCurrentPageQueryString(inquiryResponse),
+                GetPreviousPageQueryString(inquiryResponse),
+                GetNextPageQueryString(inquiryResponse));
         }
 
         public virtual IEnumerable<Task> GetTasks(IEnumerable<Data.Entities.Task> taskEntities)
         {
             var tasks = taskEntities.Select(x => _autoMapper.Map<Task>(x)).ToList();
 
+            tasks.ForEach(x =>
+            {
+                _taskLinkService.AddSelfLink(x);
+                _taskLinkService.AddLinksToChildObjects(x);
+            });
+
             return tasks;
+        }
+
+        public virtual string GetCurrentPageQueryString(PagedTaskDataInquiryResponse inquiryResponse)
+        {
+            return
+                string.Format(QueryStringFormat,
+                    inquiryResponse.PageNumber,
+                    inquiryResponse.PageSize);
+        }
+
+        public virtual string GetPreviousPageQueryString(PagedTaskDataInquiryResponse inquiryResponse)
+        {
+            return
+                string.Format(QueryStringFormat,
+                    inquiryResponse.PageNumber - 1,
+                    inquiryResponse.PageSize);
+        }
+
+        public virtual string GetNextPageQueryString(PagedTaskDataInquiryResponse inquiryResponse)
+        {
+            return string.Format(QueryStringFormat,
+                inquiryResponse.PageNumber + 1,
+                inquiryResponse.PageSize);
         }
     }
 }
